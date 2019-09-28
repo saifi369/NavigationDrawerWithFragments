c3b4298fbde02f5eb60128829f4c17529da61cda
M Saif Ullah <chsaif369@gmail.com>
2019-09-29 04:15:36 +0500

    Initial commit-Contains menu & drawer layout(Shows a Navigation Drawer in App)

---
+++ app/src/main/java/com/saifi369/navigationdrawerwithfragments/MainActivity.java
@@ -0,0 +1,14 @@
+package com.saifi369.navigationdrawerwithfragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_drawer_layout);
+    }
+}
