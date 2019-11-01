4850e1cafd79fc34982c3dd454d037372aa2359b
M Saif Ullah <chsaif369@gmail.com>
2019-11-01 22:21:26 +0500

    Handling Navigation Drawer Item Click & Changing Fragments

--- app/src/main/java/com/saifi369/navigationdrawerwithfragments/MainActivity.java
+++ app/src/main/java/com/saifi369/navigationdrawerwithfragments/MainActivity.java
@@ -1,14 +1,20 @@
 package com.saifi369.navigationdrawerwithfragments;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mNavDrawer;
 
@@ -21,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mNavDrawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, mNavDrawer, toolbar,
@@ -31,6 +38,18 @@ public class MainActivity extends AppCompatActivity {
         mNavDrawer.addDrawerListener(toggle);
 
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new InboxFragment())
+                    .commit();
+
+            navigationView.setCheckedItem(R.id.nav_inbox);
+
+
+        }
+
 
     }
 
@@ -42,5 +61,40 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_inbox:
+                //add the inbox fragment
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new InboxFragment())
+                        .commit();
+                break;
+            case R.id.nav_outbox:
+                //add the inbox fragment
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new OutboxFragment())
+                        .commit();
+                break;
+            case R.id.nav_trash:
+                //add the inbox fragment
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new TrashFragment())
+                        .commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "This is Share Item", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_help:
+                Toast.makeText(this, "This is Help Item", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        mNavDrawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
---
+++ app/src/main/java/com/saifi369/navigationdrawerwithfragments/TrashFragment.java
@@ -0,0 +1,26 @@
+package com.saifi369.navigationdrawerwithfragments;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TrashFragment extends Fragment {
+
+
+    public TrashFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_trash, container, false);
+    }
+
+}
