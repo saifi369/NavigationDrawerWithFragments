026c833d7ce59abeff161475f4b20eaaffbc25b3
M Saif Ullah <chsaif369@gmail.com>
2019-11-01 21:37:43 +0500

    Toolbar with Hamburger Icon & Transparent Status bar for Navigation Drawer

--- app/src/main/java/com/saifi369/navigationdrawerwithfragments/MainActivity.java
+++ app/src/main/java/com/saifi369/navigationdrawerwithfragments/MainActivity.java
@@ -1,14 +1,46 @@
 package com.saifi369.navigationdrawerwithfragments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mNavDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_drawer_layout);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mNavDrawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mNavDrawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+
+        mNavDrawer.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (mNavDrawer.isDrawerOpen(GravityCompat.START)) {
+            mNavDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
